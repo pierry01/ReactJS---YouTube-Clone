8ae2d01bacec9b0ce106e301e155e337516a5019
Jean Mendonça <pierrybm@gmail.com>
2020-09-29 14:28:18 +0000

    Added Drawer

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { makeStyles, AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
+import { 
+  makeStyles, AppBar, Toolbar, IconButton, Typography, Button, Drawer, List,
+  ListItem, ListItemText, Divider
+} from "@material-ui/core";
 
 import MenuIcon from '@material-ui/icons/Menu'
 import AccountCircle from '@material-ui/icons/AccountCircle'
@@ -9,10 +12,18 @@ import VideoCall from '@material-ui/icons/VideoCall'
 
 const useStyles = makeStyles((theme) => ({
   root: { height: '100vh' },
-  appBar: { boxShadow: 'none' },
+  appBar: { 
+    boxShadow: 'none',
+    zIndex: theme.zIndex.drawer + 1
+  },
   grow: { flexGrow: 1 },
   icons: { padding: theme.spacing(2) },
-  logo: { height: 25 }
+  logo: { height: 25 },
+  drawer: {
+    width: 240,
+    flexShrink: 0
+  },
+  drawerPaper: { width: 240 }
 }));
 
 const Home = () => {
@@ -47,6 +58,34 @@ const Home = () => {
           </Button>
         </Toolbar>
       </AppBar>
+      
+      
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Drawer>
     </div>
   )
 };
