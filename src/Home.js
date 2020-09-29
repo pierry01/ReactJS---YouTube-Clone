09b442bb25b0ddcdf2e2f7f81d612c8e9bf25297
Jean Mendonça <pierrybm@gmail.com>
2020-09-29 14:18:50 +0000

    Header OK

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,54 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, AppBar, Toolbar, IconButton, Typography, Button } from "@material-ui/core";
 
-const useStyles = makeStyles({
-  root: {
-    background: "red",
-    height: "100vh",
-  },
-});
+import MenuIcon from '@material-ui/icons/Menu'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+import AppsIcon from '@material-ui/icons/Apps'
+import MoreVert from '@material-ui/icons/MoreVert'
+import VideoCall from '@material-ui/icons/VideoCall'
+
+const useStyles = makeStyles((theme) => ({
+  root: { height: '100vh' },
+  appBar: { boxShadow: 'none' },
+  grow: { flexGrow: 1 },
+  icons: { padding: theme.spacing(2) },
+  logo: { height: 25 }
+}));
 
 const Home = () => {
   const classes = useStyles();
 
-  return <div className={classes.root}></div>;
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appBar} color="inherit">
+        <Toolbar>
+          <IconButton className={classes.icons} color="inherit">
+            <MenuIcon />
+          </IconButton>
+          
+          <img src="/images/preto.png" alt="Logo" className={classes.logo} />
+
+          <div className={classes.grow} />
+          
+          <IconButton className={classes.icons} color="inherit">
+            <AppsIcon />
+          </IconButton>
+          
+          <IconButton className={classes.icons} color="inherit">
+            <MoreVert />
+          </IconButton>
+          
+          <IconButton className={classes.icons} color="inherit">
+            <VideoCall />
+          </IconButton>
+
+          <Button startIcon={<AccountCircle />} variant="outlined" color="secondary">
+            Fazer Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 };
 
 export default Home;
